1c94d0a4990c086f671f3e6f133b48ba740a7ceb
MaulanaImamMuttaqin <maulanaimamuttaqin2000@gmail.com>
2022-01-11 23:57:10 +0700

    dkk;aad

--- src/pages/ContentComponents/TypingFieldStyles.js
+++ src/pages/ContentComponents/TypingFieldStyles.js
@@ -9,64 +9,120 @@ function TypingField() {
         HLword: words[0],
         wordTyped: []
     })
-
-    const letterRef = useRef(words.map(w => {
-        createRef()
-    }))
-
-    const spacePressed = useKeyPress(" ")
+    const [focused, setFocused] = useState(true)
+    //refs
+    const letterRef = useRef(words.map(() => createRef()))
     const inputRef = useRef()
+
+    //keypress
+    const spacePressed = useKeyPress(" ")
+
+
+    //Effects Hooks
+    // on space bar pressed
     useEffect(() => {
-        console.log(words.length)
-        console.log(letterRef.current)
         if (spacePressed) {
             setTypingState({
                 ...typingState,
                 wordPos: typingState.wordPos - 50,
                 HLIndex: typingState.HLIndex + 1,
-                HLword: words[typingState.HLIndex],
+                HLword: words[typingState.HLIndex + 1],
             })
+            checkTypedWord(typingState.wordTyped.join(""), typingState.HLword)
 
+            // checkTypedWord(typingState)
             inputRef.current.value = ""
+
         }
         inputRef.current.focus()
     }, [spacePressed])
 
+    // on input focus state changes
+    useEffect(() => {
+
+        console.log(focused, focused ? 'input is focus' : 'input is not focus')
+    }, [focused])
+
+
+
+    // Functions
+    // function to manage input 
     const inputHandler = (e) => {
+        let letters = letterRef.current[typingState.HLIndex].current.childNodes
         let word = e.target.value.trim()
         let word_arr = word.split("")
         setTypingState({ ...typingState, wordTyped: word_arr })
 
-        // let regexStr = `^${word}`
-        // const regex = new RegExp(regexStr, "gi");
-        // let result = regex.test(words[HLindex]);
 
-        // console.log(word, words[HLindex], result)
+        letters.forEach((l, i) => {
+            if (word_arr[i] === undefined) {
+                l.style.color = "white"
+            } else {
+                if (l.innerText === word_arr[i]) {
+                    l.style.color = '#22b005'
+                } else {
+                    l.style.color = 'red'
+                }
+            }
+        })
+    }
+
+    // check Word Wrong Or Correct
+    const checkTypedWord = (typed, answer) => {
+        if (typed.trim() !== answer) {
+            letterRef.current[typingState.HLIndex].current.style.borderBottom = "2px solid #ad070f"
+
+            console.log("wrong", `'${typed}' || ${answer}`)
+        } else {
+            console.log("correct", `'${typed}' || ${answer}`)
+        }
+    }
+
+    const focusInput = () => {
+        if (!focused) {
+            inputRef.current.focus()
+        }
 
     }
 
+    // functions to manage when input is not focus and not
+    const onFocus = () => setFocused(true)
+    const onBlur = () => setFocused(false)
+
+
+
 
     return (
-        <div className='h-screen center bg-dark-blue-gradient flex-col'>
-            <div className="h-[350px] w-[400px] overflow-hidden ">
+        <div className='h-screen center bg-dark-blue-gradient flex-col relative'>
+            {
+                !focused && <div className="h-screen w-screen center absolute z-2 " onClick={focusInput}>
+                    <p className='text-2xl font-semibold text-gray-50'>clicked anywhere to continue typing</p>
+                </div>
+            }
+            <div className={`h-[350px] w-[400px] overflow-hidden relative -z-1 ${!focused && 'blur-sm'}`}>
                 <div className='flex flex-col font-Courier tracking-[.5em] text-gray-200 relative transition-all' style={{ top: `${typingState.wordPos}px` }}>
                     {
                         words.map((w, i) => {
-                            return <WordtoLetters ref={letterRef.current[i]} key={i} index={i} word={w} HLindex={typingState.HLIndex} />
+                            return <div key={i} className={`transition-all text-center h-[50px] center word-${i === typingState.HLIndex ? 'highlight' : 'normal'} font-semibold`}>
+                                <div ref={letterRef.current[i]} className="">
+                                    {w.split("").map((l, i) => {
+                                        return <span key={i}>{l}</span>
+                                    })}
+
+                                </div>
+                            </div>
                         })
+
                     }
                 </div>
             </div>
-            <input type="text" ref={inputRef} onChange={e => inputHandler(e)} />
+            <input className='block h-0' type="text" ref={inputRef} onChange={e => inputHandler(e)} onFocus={onFocus} onBlur={onBlur} />
+
         </div >
     )
 }
 
-const WordtoLetters = ({ index, word, HLindex }) => {
-    let letters = word.split("")
 
-    return
-}
 
 const useKeyPress = targetKey => {
     const [keyPressed, setKeyPressed] = useState(false);

70e32331debe584755c5f222f5c06e9097b90817
MaulanaImamMuttaqin <maulanaimamuttaqin2000@gmail.com>
2022-01-14 15:42:25 +0700

    jfkhshj

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,18 +1,22 @@
 import React, { createContext, useReducer } from 'react'
-
 import auth from './reducers/auth'
+import typingField from './reducers/typingField'
+import words from './reducers/words'
 import authStates from './inisitalStates/authStates'
+import typingFieldStates from './inisitalStates/typingFieldStates'
+import wordStates from './inisitalStates/wordsStates'
 
 export const GlobalContext = createContext()
 
 export default function ContextProvider({ children }) {
     const [authState, authDispatch] = useReducer(auth, authStates)
-
+    const [typingFieldState, typingDispatch] = useReducer(typingField, typingFieldStates)
+    const [wordsState, wordsDispatch] = useReducer(words, wordStates)
 
     return (
         <GlobalContext.Provider
             value={{
-                authState, authDispatch
+                authState, typingFieldState, wordsState, authDispatch, typingDispatch, wordsDispatch
             }}
         >
             {children}
